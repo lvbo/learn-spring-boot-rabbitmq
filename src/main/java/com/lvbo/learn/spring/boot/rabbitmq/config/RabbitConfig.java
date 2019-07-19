d0c71f0d9af7765eb460d1638eedcf3437f3d26d
lvbo <lvbo09@gmail.com>
2019-07-19 14:38:58 +0800

    增加一些queue和exchange的绑定测试

--- src/main/java/com/lvbo/learn/spring/boot/rabbitmq/config/RabbitConfig.java
+++ src/main/java/com/lvbo/learn/spring/boot/rabbitmq/config/RabbitConfig.java
@@ -1,9 +1,6 @@
 package com.lvbo.learn.spring.boot.rabbitmq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -49,5 +46,60 @@ public class RabbitConfig {
     @Bean
     public Binding bindingExchangeC(Queue queueA, FanoutExchange fanoutExchangeC) {
         return BindingBuilder.bind(queueA).to(fanoutExchangeC);
+    }
+
+    @Bean
+    public DirectExchange directExchangeA() {
+        return new DirectExchange("direct_exchange_a");
+    }
+
+    @Bean
+    public DirectExchange directExchangeB() {
+        return new DirectExchange("direct_exchange_b");
+    }
+
+    @Bean
+    public DirectExchange directExchangeC() {
+        return new DirectExchange("direct_exchange_c");
+    }
+
+    @Bean
+    public Queue queueTest1() {
+        return new Queue("queue_test_1");
+    }
+
+    @Bean
+    public Queue queueTest2() {
+        return new Queue("queue_test_2");
+    }
+
+    @Bean
+    public Queue queueTest3() {
+        return new Queue("queue_test_3");
+    }
+
+    @Bean
+    public Queue queueTest4() {
+        return new Queue("queue_test_4");
+    }
+
+    @Bean
+    public Binding bindingToDirectExchangeA(Queue queueTest1, DirectExchange directExchangeA) {
+        return BindingBuilder.bind(queueTest1).to(directExchangeA).with("routing_1");
+    }
+
+    @Bean
+    public Binding bindingToDirectExchangeA2(Queue queueTest2, DirectExchange directExchangeA) {
+        return BindingBuilder.bind(queueTest2).to(directExchangeA).with("routing_1");
+    }
+
+    @Bean
+    public Binding bindingToDirectExchangeB(Queue queueTest3, DirectExchange directExchangeB) {
+        return BindingBuilder.bind(queueTest3).to(directExchangeB).with("routing_1");
+    }
+
+    @Bean
+    public Binding bindingToDirectExchangeC(Queue queueTest4, DirectExchange directExchangeC) {
+        return BindingBuilder.bind(queueTest4).to(directExchangeC).with("routing_2");
     }
 }
--- src/test/java/com/lvbo/learn/spring/boot/rabbitmq/service/RabbitSenderTest.java
+++ src/test/java/com/lvbo/learn/spring/boot/rabbitmq/service/RabbitSenderTest.java
@@ -2,6 +2,7 @@ package com.lvbo.learn.spring.boot.rabbitmq.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,8 +14,18 @@ public class RabbitSenderTest {
     @Autowired
     private RabbitSender rabbitSender;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Test
     public void sendMsg() {
         rabbitSender.send();
+    }
+
+    @Test
+    public void testSendRouting1() {
+//        rabbitTemplate.convertAndSend("direct_exchange_a","routing_1", "test1");
+//        rabbitTemplate.convertAndSend("routing_1", "test1");
+        rabbitTemplate.convertAndSend("direct_exchange_c","routing_2", "test1");
     }
 }
